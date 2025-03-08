07ecb08b7f09e5fbc210bea09617b4e209e35a2b
Aditya Vaish <adityavaish846@gmail.com>
2025-03-08 16:03:14 +0530

    Update block_stacks.cpp

--- block_stacks.cpp
+++ block_stacks.cpp
@@ -1,20 +1,21 @@
 /*Block-Stacks*/
 //A Tetris Based game made in pure C++
 //Made by vaishcodescape and sam5506
+
 #include <iostream>
 #include <vector>
-#include <cstring> // For memcpy()
-#include <cstdlib> // For rand()
-#include <ctime>   // For seeding random
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
 
 #ifdef _WIN32
-    #include <conio.h>   // For _kbhit() and _getch()
-    #include <windows.h> // For Sleep()
+    #include <conio.h>
+    #include <windows.h>
 #else
-    #include <unistd.h>  // For usleep()
-    #include <termios.h> // For non-blocking keyboard input
+    #include <unistd.h>
+    #include <termios.h>
     #include <fcntl.h>
-
+    
     int _kbhit() {
         struct termios oldt, newt;
         int ch;
@@ -36,7 +37,6 @@
             ungetc(ch, stdin);
             return 1;
         }
-
         return 0;
     }
 
@@ -51,80 +51,57 @@
         tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
         return ch;
     }
-
+    
     void Sleep(int ms) {
-        usleep(ms * 1000); // Convert milliseconds to microseconds
+        usleep(ms * 1000);
     }
 #endif
 
 using namespace std;
 
-// Game board size
-const int WIDTH = 10;
-const int HEIGHT = 20;
-int board[HEIGHT][WIDTH] = {0}; // 0 means empty, 1 means filled
-
-// Tetromino shapes (4x4 representation)
-const int TETROMINOS[7][4][4] = {
-    {{0,0,0,0}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}},  // I
-    {{1,1,0,0}, {1,1,0,0}, {0,0,0,0}, {0,0,0,0}},  // O
-    {{0,1,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}},  // T
-    {{0,1,1,0}, {1,1,0,0}, {0,0,0,0}, {0,0,0,0}},  // S
-    {{1,1,0,0}, {0,1,1,0}, {0,0,0,0}, {0,0,0,0}},  // Z
-    {{1,0,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}},  // J
-    {{0,0,1,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}}   // L
+const int WIDTH = 10, HEIGHT = 20;
+int board[HEIGHT][WIDTH] = {0};
+const int TETROMINOS[7][4][4] = { 
+    {{0,0,0,0}, {1,1,1,1}, {0,0,0,0}, {0,0,0,0}},
+    {{1,1,0,0}, {1,1,0,0}, {0,0,0,0}, {0,0,0,0}},
+    {{0,1,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}},
+    {{0,1,1,0}, {1,1,0,0}, {0,0,0,0}, {0,0,0,0}},
+    {{1,1,0,0}, {0,1,1,0}, {0,0,0,0}, {0,0,0,0}},
+    {{1,0,0,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}},
+    {{0,0,1,0}, {1,1,1,0}, {0,0,0,0}, {0,0,0,0}}
 };
 
-// Tetromino structure
 struct Tetromino {
     int shape[4][4];
-    int x, y; // Position
-
-    // Constructor
+    int x, y;
+    
     Tetromino(int type) {
         memcpy(shape, TETROMINOS[type], sizeof(shape));
         x = WIDTH / 2 - 2;
         y = 0;
     }
-
-    // Rotate tetromino 90 degrees
+    
     void rotate() {
         int temp[4][4] = {0};
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
-                temp[j][3 - i] = shape[i][j]; 
+                temp[j][3 - i] = shape[i][j];
         memcpy(shape, temp, sizeof(shape));
     }
 };
 
-// Draw the game board and tetromino
-void draw(Tetromino &t) {
-    system("clear"); // Use "cls" on Windows
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            bool isTetromino = false;
-            for (int r = 0; r < 4; r++)
-                for (int c = 0; c < 4; c++)
-                    if (t.shape[r][c] && i == t.y + r && j == t.x + c)
-                        isTetromino = true;
-            if (isTetromino)
-                cout << "# ";
-            else if (board[i][j])
-                cout << "* ";
-            else
-                cout << ". ";
-        }
-        cout << endl;
-    }
+bool isValidMove(Tetromino &t, int dx, int dy) {
+    for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            if (t.shape[i][j]) {
+                int newX = t.x + j + dx;
+                int newY = t.y + i + dy;
+                if (newX < 0 || newX >= WIDTH || newY >= HEIGHT || board[newY][newX])
+                    return false;
+            }
+    return true;
 }
 
-// Move tetromino
-void moveTetromino(Tetromino &t, int dx, int dy) {
-    t.x += dx;
-    t.y += dy;
-}
-
-// Place tetromino on board
 void placeTetromino(Tetromino &t) {
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
@@ -132,35 +109,64 @@ void placeTetromino(Tetromino &t) {
                 board[t.y + i][t.x + j] = 1;
 }
 
-// Main game loop
-void gameLoop() {
-    srand(time(0));
-    Tetromino currentTetromino(rand() % 7);
-
-    while (true) {
-        draw(currentTetromino);
-
-        if (_kbhit()) {
-            char key = _getch();
-            if (key == 'a') moveTetromino(currentTetromino, -1, 0);
-            if (key == 'd') moveTetromino(currentTetromino, 1, 0);
-            if (key == 'w') currentTetromino.rotate();
-            if (key == 's') moveTetromino(currentTetromino, 0, 1);
-        }
-
-        // Move tetromino down every second
-        Sleep(1000);
-        moveTetromino(currentTetromino, 0, 1);
-
-        // Collision check (basic)
-        if (currentTetromino.y >= HEIGHT - 4) {
-            placeTetromino(currentTetromino);
-            currentTetromino = Tetromino(rand() % 7); // Generate a new tetromino
+void clearRows() {
+    for (int i = HEIGHT - 1; i >= 0; i--) {
+        bool full = true;
+        for (int j = 0; j < WIDTH; j++)
+            if (!board[i][j]) full = false;
+        if (full) {
+            for (int k = i; k > 0; k--)
+                memcpy(board[k], board[k - 1], sizeof(board[k]));
+            memset(board[0], 0, sizeof(board[0]));
+            i++;
         }
     }
 }
 
-// Entry point
+void draw(Tetromino &t) {
+    system("clear");
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            bool isTetromino = false;
+            for (int r = 0; r < 4; r++)
+                for (int c = 0; c < 4; c++)
+                    if (t.shape[r][c] && i == t.y + r && j == t.x + c)
+                        isTetromino = true;
+            cout << (isTetromino ? "# " : board[i][j] ? "* " : ". ");
+        }
+        cout << endl;
+    }
+}
+
+void gameLoop() {
+    srand(time(0));
+    Tetromino current(rand() % 7);
+
+    while (true) {
+        draw(current);
+        if (_kbhit()) {
+            char key = _getch();
+            if (key == 'a' && isValidMove(current, -1, 0)) current.x--;
+            if (key == 'd' && isValidMove(current, 1, 0)) current.x++;
+            if (key == 'w') {
+                current.rotate();
+                if (!isValidMove(current, 0, 0)) current.rotate(); 
+            }
+            if (key == 's' && isValidMove(current, 0, 1)) current.y++;
+        }
+        Sleep(200);
+        if (!isValidMove(current, 0, 1)) {
+            placeTetromino(current);
+            clearRows();
+            current = Tetromino(rand() % 7);
+            if (!isValidMove(current, 0, 0)) {
+                cout << "Game Over!" << endl;
+                break;
+            }
+        } else current.y++;
+    }
+}
+
 int main() {
     gameLoop();
     return 0;
